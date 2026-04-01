95011a6474ce9b21859d4e3b9a3df00b6db08040
devyouth94 <devyouth94@gmail.com>
2026-04-01 18:42:00 +0900

    feat: Expo 초기 구조 정리 및 린트 설정 추가

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+const { defineConfig } = require("eslint/config");
+const eslintConfigPrettier = require("eslint-config-prettier");
+const expoConfig = require("eslint-config-expo/flat");
+const eslintPluginPrettier = require("eslint-plugin-prettier");
+const simpleImportSort = require("eslint-plugin-simple-import-sort");
+
+module.exports = defineConfig([
+  expoConfig,
+  eslintConfigPrettier,
+  {
+    ignores: ["dist/*", ".expo/*", "node_modules/*", "ios/*", "android/*"],
+    plugins: {
+      prettier: eslintPluginPrettier,
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": "warn",
+      "arrow-body-style": "off",
+      "prefer-arrow-callback": "off",
+      "prettier/prettier": "error",
+      "simple-import-sort/imports": [
+        "error",
+        {
+          groups: [
+            ["^react", "^expo", "^@?\\w"],
+            ["^~/"],
+            ["^\\./", "^\\.\\./"],
+            ["^\\.css$"],
+          ],
+        },
+      ],
+    },
+  },
+]);
